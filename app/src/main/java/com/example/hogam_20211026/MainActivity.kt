ef609233e08bbb9a7478e0cd34a79855b6c54a1a
김다은 <softeun92@naver.com>
2021-10-26 12:53:57 +0900

    데이터바인딩처리

--- app/src/main/java/com/example/hogam_20211026/MainActivity.kt
+++ app/src/main/java/com/example/hogam_20211026/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.hogam_20211026
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.hogam_20211026.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
     }
 }
